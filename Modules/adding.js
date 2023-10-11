4157135382c692a72c4e293f12f6344bf8a709d9
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-11 12:51:15 +0200

    update scripts.js

--- Modules/adding.js
+++ Modules/adding.js
@@ -1,5 +1,10 @@
 //@ts-check
 
+/**
+ * @typedef {'high' | 'medium' | 'low'} Urgency - The priority that the tasks
+ * should take in terms of how quickly it should be completed
+ */
+
 import { getHtml } from "./helpers.js";
 
 const createAddingHtml = () => {
@@ -16,14 +21,32 @@ const createAddingHtml = () => {
   dialog.innerHTML = `<dialog class="overlay" open="open">
 <h2 class="overlay__title">Add Task</h2>
 
+<form data-form id="adding">
 <label class="overlay__field">
   <div>Title</div>
-  <input class="overlay__input" />
+  <input required class="overlay__input" name="title"/>
 </label>
+
+<label class="overlay__field">
+  <div>due</div>
+  <input type="date" class="overlay__input" name="due"/>
+</label>
+
+<label class="overlay__field">
+  <div>urgency</div>
+  <select required class="overlay__input" name="urgency">
+  <option value="medium">Medium</option>
+  <option value="high">High</option>
+  <option value="low">Low</option>
+  
+
+  </select>
+</label>
+</form>
 
 <div class="overlay__row">
   <button class="button" data-cancel>Cancel</button>
-  <button class="button" data-save >Save</button>
+  <button class="button" type="submit" form="adding">Save</button>
 </div>
 </dialog>
 `;
@@ -31,16 +54,67 @@ const createAddingHtml = () => {
   return {
     button,
     dialog,
-    save: dialog.querySelector("[data-save ]"),
     cancel: dialog.querySelector("[data-cancel]"),
+    form: dialog.querySelector("[data-form]"),
   };
 };
+/**
+ * @typedef {object} Data
+ * @prop {string } title
+ * @prop {Date | null } due
+ * @prop {Urgency} urgency
+ */
 
+/**
+ * @callback Submission
+ * @param {Data} data
+ */
+
+/**
+ *@typedef {object} adding
+ @prop {Submission} submission
+ * 
+ */
+
+ /**
+  * 
+  * @returns {adding}
+  */
 export const createAdding = () => {
-  const { dialog, button, cancel, save } = createAddingHtml();
+  const { dialog, button, cancel, form } = createAddingHtml();
+  const state = {
+    submission : undefined
+  };
+
   button.addEventListener("click", () => {
-    dialog.open = true;
+    dialog.showModal();
   });
+  cancel.addEventListener("click", () => {
+    dialog.close();
+  });
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if ( typeof state.submission !== "function"){
+      throw new Error ("submission value has to be set a function ")
+    }
+
+    if (!(event.target instanceof HTMLFormElement)) {throw new Error ('form not found')
+
+    }
+    const entries = new FormData (event.target)
+    const response= Object.fromEntries(entries)
+    state.submission(response)
+event.target.reset()
+    dialog.close();
+  });
+  return {
+
+    get submission (){
+return state.submission
+    },
+    set submission (newValue)
+    { state.submission= newValue}
+  }
 };
 
 export default createAdding;
